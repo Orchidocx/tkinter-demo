0bfaaa842669b802ee8a04d61e202342155b5ac5
Jacky Li <ilikemacaronii@gmail.com>
2021-03-16 16:28:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-import tkinter
+from tkinter import *
+# import tkinter
 
 if __name__ == '__main__':
-    window = tkinter.Tk()
+    window = Tk()
     window.title("Tkinter GUI")
     window.minsize(width=500,height=300)
 
     # Label
-    my_label = tkinter.Label(text="I am a label")
+    my_label = Label(text="I am a label", font=("Courier New", 20, "bold"))
+    my_label.pack()
+    my_label["text"] = "new text"   # changing the text
+    my_label.config(text="newer text")  # another way to change text
+
+    # Button
+    button = Button(text="hello")
 
     # end statement
     window.mainloop()
