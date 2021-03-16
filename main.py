51b2e0db6a93ef72ca17ef31c686fd7b374325f1
Jacky Li <ilikemacaronii@gmail.com>
2021-03-16 16:50:03 -0700

    added widget demos

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import *
 if __name__ == '__main__':
     window = Tk()
     window.title("Tkinter GUI")
-    window.minsize(width=500,height=300)
+    window.minsize(width=500,height=400)
 
     # Label
     my_label = Label(text="I am a label", font=("Courier New", 20, "bold"))
@@ -25,6 +25,42 @@ if __name__ == '__main__':
     u_input = Entry(width=10)
     u_input.pack()
 
+    # Text (Entry)
+    txt = Text(width=30, height=5)
+    txt.focus() # focuses into text entry
+    txt.insert(END, "Example of multi-line text entry.")
+    txt.pack()
+
+    # Spinbox
+    spinbox = Spinbox(from_=0, to=10, width=10)
+    spinbox.pack()
+
+    # Scale
+    scale = Scale(from_=1, to=10)
+    scale.pack()
+
+    # Checkbutton
+    checked_state = IntVar()
+    c_button = Checkbutton(text="isOn?", variable=checked_state)
+    c_button.pack()
+
+    # Radio Button
+    radio_state = IntVar()
+    r_button1 = Radiobutton(text="optionA", value=1, variable=radio_state)
+    r_button2 = Radiobutton(text="optionB", value=2, variable=radio_state)
+    r_button1.pack()
+    r_button2.pack()
+
+    # Listbox
+    def listbox_used(evt):
+        # Gets curr selection from listbox
+        print(listbox.get(listbox.curselection()))
+    listbox = Listbox(height=4)
+    fruits = ["Apples", "Bananas", "Cherries", "Durian"]
+    for fruit in fruits:
+        listbox.insert(fruits.index(fruit), fruit)
+    listbox.bind("<<ListboxSelect>>", listbox_used)
+    listbox.pack()
 
     # end statement
     window.mainloop()
